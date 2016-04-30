00809e1950b6ecd02e9f4385ca289c3277ebf3e7
lers92 <lers92@users.noreply.github.com>
2016-04-30 00:54:26 -0600

    Update DataSource.groovy
    Change in data source

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,8 @@
 dataSource {
     pooled = true
-    driverClassName = "org.postgresql.Driver"
-    username = "ennhgfqguwkgvr"
-    password = "erfiKOFNuUjiUiTgxxSun4bIg1"
+//    driverClassName = "com.mysql.jdbc.Driver"
+//    username = "root"
+//    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -12,32 +12,44 @@ hibernate {
 // environment specific settings
 environments {
     development {
-        dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:postgresql://ennhgfqguwkgvr:erfiKOFNuUjiUiTgxxSun4bIg1@ec2-54-225-94-145.compute-1.amazonaws.com:5432/d5s0lnrks94gm0"
-        }
+//        dataSource {
+//            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+//            zzurl = "jdbc:mysql://localhost/subastas"
+//        }
+        dbCreate = "create"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            url = 'postgres://ennhgfqguwkgvr:erfiKOFNuUjiUiTgxxSun4bIg1@ec2-54-225-94-145.compute-1.amazonaws.com:5432/d5s0lnrks94gm0 '
+            username = 'ennhgfqguwkgvr'
+            password = 'erfiKOFNuUjiUiTgxxSun4bIg1'
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:postgresql://ennhgfqguwkgvr:erfiKOFNuUjiUiTgxxSun4bIg1@ec2-54-225-94-145.compute-1.amazonaws.com:5432/d5s0lnrks94gm0"
+            url = "jdbc:mysql://localhost/subastas"
         }
     }
     production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:postgresql://ennhgfqguwkgvr:erfiKOFNuUjiUiTgxxSun4bIg1@ec2-54-225-94-145.compute-1.amazonaws.com:5432/d5s0lnrks94gm0"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
+//        dataSource {
+//            dbCreate = "update"
+//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            pooled = true
+//            properties {
+//               maxActive = -1
+//               minEvictableIdleTimeMillis=1800000
+//               timeBetweenEvictionRunsMillis=1800000
+//               numTestsPerEvictionRun=3
+//               testOnBorrow=true
+//               testWhileIdle=true
+//               testOnReturn=true
+//               validationQuery="SELECT 1"
+//            }
+//        }
+            dbCreate = "create"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            url = 'postgres://ennhgfqguwkgvr:erfiKOFNuUjiUiTgxxSun4bIg1@ec2-54-225-94-145.compute-1.amazonaws.com:5432/d5s0lnrks94gm0 '
+            username = 'ennhgfqguwkgvr'
+            password = 'erfiKOFNuUjiUiTgxxSun4bIg1'
     }
 }
